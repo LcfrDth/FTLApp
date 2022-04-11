a4c237d2cb1e248ee78087bd29dfa02603e972aa
Isaac Sarte <aizakku09@gmail.com>
2022-04-11 17:06:58 +0800

    setup commerce js and get products from the api

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-/* Components */
-import { list } from '../subComponents/ProductList';
+// Commerce JS
+import { commerce } from '../lib/Commerce';
+
+/* Icons */
+import { AddtoCartIcon } from '../subComponents/AllSVG';
 
 const Products = () => {
+
+    /* State Management */
+    const [products, setProducts] = useState([]);
+
+    const getProducts = async () => {
+        const response = await commerce.products.list();
+        setProducts((response && response.data) || []);
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    console.log({ products });
+
     return (
         <>
             <h1 className="font-bold text-4xl text-center uppercase text-[#57ADAD]">Our products</h1>
             <br /><br /><br /><br />
             <div className="products__container max-w-7xl w-full grid grid-cols-4 gap-4 text-center justify-items-center custom:grid-cols-2 w-[90%]">
-                {list.map((item) => (
+                {products.map((item) => (
                     <div className="flex flex-col">
                         <div>
-                            <img src={item.image} alt="product image" />
+                            <img src={item.image.url} alt="product image" />
                         </div>
                         <h2 className="capitalize font-[750] mt-4 text-xl text-gray-800 tracking-wide">{item.name}</h2>
                         <br />
-                        <p className="category__name bg-[#829536] text-white font-semibold w-1/2 rounded-lg text-lg text-center">{item.category}</p>
+                        <span className="font-semibold text-gray-700 text-xl">{item.price.formatted_with_symbol}</span>
                         <br />
-                        <span className="font-semibold text-gray-700 text-xl">{item.price}</span>
+                        <div className="add__cart__products--btn flex justify-center align-items-center gap-4 cursor-pointer">
+                            <AddtoCartIcon /> <span className="font-semibold text-lg text-gray-700">Add to Cart</span>
+                        </div>
+                        <br />
                     </div>
                 ))}
             </div>
