f3edc2d2417d1e1788f5f779513ef7a6a2aac17f
Argie Barcena <barcenaargie@gmail.com>
2022-04-20 01:01:27 +0800

    FIX/cart_component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+// import Header from '../Header'
+import CustomCart from '../Cart/CustomCart'
 
-const Cart = ({ cartData, updateData, handleEmptyData, removeItem }) => {
-    return (
-        <div>Cart Component</div>
-    )
+
+const Cart = ({setCartData, updateData, handleEmptyData, removeItem }) => {
+
+    const [lineItems, setLineItems] = useState([]);
+    
+
+    useEffect(() => {
+        setLineItems(JSON.parse(localStorage.getItem("cartItem")))
+    }, []);
+    
+    console.log(lineItems);
+    return ( 
+        <>
+        {/* <Header /> */}
+        <div id="basket">
+            <CustomCart 
+                lineItems={lineItems}
+                updateData={updateData}
+
+            />
+            <div className="actions">
+                <button
+                    onClick={handleEmptyData}
+                >
+                    Empty Cart
+                </button>
+                <button>
+                    Checkout
+                </button>
+            </div>
+        </div>
+        </>
+    );   
 }
 
 export default Cart;
